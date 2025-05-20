a97e680e96e15030552a8801bf529d014a5c9bd7
sergei-pchelintsev <pchelintsev@orderofmeta.com>
2025-05-20 22:16:49 +0200

    - improved code

--- obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2bbbfbdfdf1cae72ce2e15aa3ce2c5461420bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
